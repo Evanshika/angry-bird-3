4e782c2f3a9b3de798e2efcb21d5380cd5c145c4
unknown <neelofer28s_@reddifmail.com>
2020-10-29 17:42:07 +0530

    sketch.js is modified

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground (150,305,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
